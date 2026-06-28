43207151fec5924906b790e3dc21ba79780cdb88
kchiang <kaizhi3006@gmail.com>
2026-06-28 23:05:02 +0800

    chroe(sql): added basic sql queries files

---
+++ sql/queries/get_content_hash.sql
@@ -0,0 +1,3 @@
+SELECT content_hash
+FROM credit_cards
+WHERE card_title = ?;
---
+++ sql/queries/open_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS credit_cards (
+    card_title			TEXT PRIMARY KEY,
+    bank				TEXT NOT NULL,
+	cashback			TEXT NOT NULL,
+	petrol				TEXT NOT NULL,
+	rewards				TEXT NOT NULL,
+	travel				TEXT NOT NULL,
+	premium_perks		TEXT NOT NULL,
+	balance_transfer	TEXT NOT NULL,
+	easy_payment_plan	TEXT NOT NULL,
+	fees				TEXT NOT NULL,
+	requirements		TEXT NOT NULL,
+	features			TEXT NOT NULL,
+	review				TEXT NOT NULL,
+	keywords			TEXT NOT NULL,
+    content_hash		TEXT NOT NULL
+);
