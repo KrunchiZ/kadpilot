d9678535e02aff2e654d4af1825027a91e8637c3
kchiang <kaizhi3006@gmail.com>
2026-06-29 00:37:37 +0800

    refactor(sql): renamed colume named returned by count_categories.sql

--- sql/count_categories.sql
+++ sql/count_categories.sql
@@ -1,6 +1,6 @@
 SELECT 
-    SUM(CASE WHEN cashback != 'N/A' THEN 1 ELSE 0 END) AS total_cashback,
-    SUM(CASE WHEN petrol != 'N/A' THEN 1 ELSE 0 END) AS total_petrol,
-    SUM(CASE WHEN rewards != 'N/A' THEN 1 ELSE 0 END) AS total_rewards,
-    SUM(CASE WHEN travel != 'N/A' THEN 1 ELSE 0 END) AS total_travel
+    SUM(CASE WHEN cashback != 'N/A' THEN 1 ELSE 0 END) AS Cashback,
+    SUM(CASE WHEN petrol != 'N/A' THEN 1 ELSE 0 END) AS Petrol,
+    SUM(CASE WHEN rewards != 'N/A' THEN 1 ELSE 0 END) AS Rewards,
+    SUM(CASE WHEN travel != 'N/A' THEN 1 ELSE 0 END) AS Travel
 FROM credit_cards;
