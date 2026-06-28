6bfac12ceb39a5deff8a2738ff8c16858e82df1b
kchiang <kaizhi3006@gmail.com>
2026-06-29 01:48:43 +0800

    feat(backend, sql):
    BREAKING CHANGE: removed keywords and review schema from ETL pipeline
    and database.

--- sql/count_categories.sql
+++ sql/count_categories.sql
@@ -1,6 +1,7 @@
 SELECT 
-    SUM(CASE WHEN cashback != 'N/A' THEN 1 ELSE 0 END) AS Cashback,
-    SUM(CASE WHEN petrol != 'N/A' THEN 1 ELSE 0 END) AS Petrol,
-    SUM(CASE WHEN rewards != 'N/A' THEN 1 ELSE 0 END) AS Rewards,
-    SUM(CASE WHEN travel != 'N/A' THEN 1 ELSE 0 END) AS Travel
+    SUM(CASE WHEN cashback != 'N/A' THEN 1 ELSE 0 END) AS "Cashback",
+    SUM(CASE WHEN petrol != 'N/A' THEN 1 ELSE 0 END) AS "Petrol",
+    SUM(CASE WHEN rewards != 'N/A' THEN 1 ELSE 0 END) AS "Rewards",
+    SUM(CASE WHEN travel != 'N/A' THEN 1 ELSE 0 END) AS "Travel",
+    SUM(CASE WHEN premium_perks != 'N/A' THEN 1 ELSE 0 END) AS "Premium Perks"
 FROM credit_cards;
